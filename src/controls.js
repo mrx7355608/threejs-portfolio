3d39e1779da0b70360b1f6f0c5211ebd4ac1068e
mrx7355608 <mrx7355608@gmail.com>
2025-02-28 16:28:31 +0500

    added orbit controls

--- src/controls.js
+++ src/controls.js
@@ -1,7 +1,12 @@
-export const setupControls = () => {
-    const controls = new FirstPersonControls(camera, renderer.domElement);
-    controls.movementSpeed = 400;
-    controls.lookSpeed = 0.05;
-    controls.lookVertical = false;
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+export const setupControls = (camera, renderer) => {
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true; // Smooth camera movement
+    // controls.screenSpacePanning = false;
+    controls.minDistance = 2;
+    controls.maxDistance = 50;
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.target.set(0, 20, -100);
     return controls;
 };
