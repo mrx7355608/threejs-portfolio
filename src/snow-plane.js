d70702801f7619dba563f77660375369453c0004
mrx7355608 <mrx7355608@gmail.com>
2025-03-01 18:41:21 +0500

    removed texture from plane

--- src/snow-plane.js
+++ src/snow-plane.js
@@ -8,20 +8,15 @@ export const SnowPlane = () => {
             normal: loader.load("textures/snow/normal.png"),
             diffuse: loader.load("textures/snow/diffuse.png"),
             displacement: loader.load("textures/snow/displacement.png"),
+            ao: loader.load("textures/snow/ao.jpg"),
         };
     };
 
     const initPlane = () => {
         const geometry = new THREE.PlaneGeometry(5000, 5000, 128, 128);
-        const { rough, normal, diffuse, displacement } = loadTexture();
-        const material = new THREE.MeshStandardMaterial({
-            map: diffuse,
-            normalMap: normal,
-            displacementMap: displacement,
-            roughnessMap: rough,
-            displacementScale: 200,
-            displacementBias: -60,
-            roughness: 2,
+        const { ao, rough, normal, diffuse, displacement } = loadTexture();
+        const material = new THREE.MeshPhongMaterial({
+            color: "white",
         });
         const mesh = new THREE.Mesh(geometry, material);
 
